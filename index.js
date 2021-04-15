eda334f7c7c61f846363edadb971e74bdd452f7c
DMawi17 <asegda17@gmail.com>
2021-04-15 09:27:11 +0200

    ternary

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ if (a + b < 4) {
 } */
 
 /* _________________________________ */
-let a=3;
-let b=6;
+let a = 3;
+let b = 6;
 
 a + b < 4 ? (result = "Below") : (result = "Over");
-//write your code here 
-console.log(result)
+//write your code here
+console.log(result);
 /* _________________________________ */
-
 
 //Rewrite the code below to use the ternary operator (?:) (you should be able to condense the if-else logic into one line).
 /* if (score > 1337)
@@ -37,11 +36,10 @@ let msg = "";
 score > 1337
     ? (msg = "This is a new highscore!")
     : (msg = "You need more points to beat the highscore!");
-//write your code here 
-console.log(msg)
+//write your code here
+console.log(msg);
 
 /* _________________________________ */
-
 
 // Rewrite if..else using multiple ternary operators '?'.
 //For readability, it’s recommended to split the code into multiple lines.
@@ -61,10 +59,15 @@ if (login == 'Employee') {
 /* _________________________________ */
 
 let login = "";
- 
-login == 'Employee' ? message = 'Hello' : login == 'Director' ? message = 'Greetings' : login == '' ? message = 'No login' : message = ''
-//write your code here 
-console.log(message)
+
+login == "Employee"
+    ? (message = "Hello")
+    : login == "Director"
+    ? (message = "Greetings")
+    : login == ""
+    ? (message = "No login")
+    : (message = "");
+//write your code here
+console.log(message);
 
 /* _________________________________ */
-
